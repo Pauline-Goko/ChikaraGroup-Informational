b2aed667ab1efdb237473eecae80d27abf1569a7
ndiranguwanjikupurity@gmail.com <ndiranguwanjikupurity@gmail.com>
2023-09-20 12:01:43 +0300

    Created teams section and tests for the component

--- chikara-informational-website/src/App.test.js
+++ chikara-informational-website/src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders team section title', () => {
+  const { getByText } = render(<App />);
+  const titleElement = getByText(/Meet the team/i);
+  expect(titleElement).toBeInTheDocument();
 });
---
+++ chikara-informational-website/src/Components/team/index.js
@@ -0,0 +1,51 @@
+
+import "./style.css";
+import TeamCard from "./props";
+
+function Team() {
+    return (
+        <div className="team-section" id="team">
+            <div className="dt-title-content">
+                <h3 className="dt-title">
+                    <span>Meet the team</span>
+                </h3>
+
+                <p className="dt-description">
+                    Meet our exceptional team of engineers, dedicated to
+                    providing top-notch services.
+                </p>
+            </div>
+
+            <div className="dt-cards-content">
+                <TeamCard
+                    img="/Assets/maria.jpg"
+                    name="Mariagoretti Kimani"
+                    title="Software Engineer"
+                />
+
+                <TeamCard
+                    img="/Assets/purity.jpeg"
+                    name="Purity Wanjiku"
+                    title="Software Engineer"
+                />
+                <TeamCard
+                    img="/Assets/kenyani.jpg"
+                    name="Rachael Kenyani"
+                    title="Software Engineer"
+                />
+                <TeamCard
+                    img="/Assets/pauline.jpg"
+                    name="Pauline Goko"
+                    title="Software Engineer"
+                />
+                <TeamCard
+                    img="/Assets/rita.jpeg"
+                    name="Rita Khaseyi"
+                    title="Software Engineer"
+                />
+            </div>
+        </div>
+    );
+}
+
+export default Team;
