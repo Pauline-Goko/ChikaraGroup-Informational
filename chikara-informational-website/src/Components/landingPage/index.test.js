5da75316b0d1df17dcde33174eac25f6846a4431
MariaGKimani <mariakimanigoretti@gmail.com>
2023-09-20 07:10:05 +0300

    created the landing page

---
+++ chikara-informational-website/src/Components/landingPage/index.test.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Hero from '.';
+
+test('renders the Hero component without errors', () => {
+  render(<Hero />);
+});
+
+
+test('displays the "YOUR PARTNER IN SUSTAINABILITY" title', () => {
+  const { getByText } = render(<Hero />);
+  const titleElement = getByText('YOUR PARTNER IN SUSTAINABILITY');
+  expect(titleElement).toBeInTheDocument();
+});
+
+test('displays the "At Chikara..." description text', () => {
+  const { getByText } = render(<Hero />);
+  const descriptionElement = getByText(
+    'At Chikara, we\'re committed to creating a sustainable future. Join us in our mission to reduce carbon emissions and make the world a greener place.'
+  );
+  expect(descriptionElement).toBeInTheDocument();
+});
+
+test('renders the "Learn More" button', () => {
+  const { getByText } = render(<Hero />);
+  const buttonElement = getByText('Learn More');
+  expect(buttonElement).toBeInTheDocument();
+});
+
+test('renders the video element with a "background-video" class', () => {
+  const { container } = render(<Hero />);
+  const videoElement = container.querySelector('.background-video');
+  expect(videoElement).toBeInTheDocument();
+});
+
+
+test('renders a text section with a "text-section" class', () => {
+  const { container } = render(<Hero />);
+  const textSection = container.querySelector('.text-section');
+  expect(textSection).toBeInTheDocument();
+});
+
+test('renders a title element with a "text-title" class', () => {
+  const { container } = render(<Hero />);
+  const titleElement = container.querySelector('.text-title');
+  expect(titleElement).toBeInTheDocument();
+});
