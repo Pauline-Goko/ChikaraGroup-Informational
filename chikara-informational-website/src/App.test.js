e8916c48b21cfb036586fb6e2a706f70336ad758
Rachel-Kenyani <rachelkenyani@gmail.com>
2023-09-20 15:23:02 +0300

    added the values section

--- chikara-informational-website/src/App.test.js
+++ chikara-informational-website/src/App.test.js
@@ -1,9 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 import About from './Components/About';
+import Value from './Components/Values';
 
 test('renders About component', () => {
   render(<About />);
   const contactElement = screen.getByText('About Us');
+  expect(contactElement).toBeInTheDocument();
+});
+test('renders Values component', () => {
+  render(<Value />);
+  const contactElement = screen.getByText('Our Values');
   expect(contactElement).toBeInTheDocument();
 });
